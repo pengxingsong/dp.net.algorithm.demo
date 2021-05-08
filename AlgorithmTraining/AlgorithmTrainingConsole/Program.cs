27d70d0f27a33e66869930d755cb190dec6f5781
pengxingsong <pengxingsong@colipu.com>
2021-05-08 17:23:35 +0800

    提交项目

---
+++ AlgorithmTraining/AlgorithmTrainingConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmTrainingConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
